2a506063f734d9352fe938072e12204f0ca7b78d
Altamash <sayyedaltamash7086@gmail.com>
2024-12-01 10:24:57 +0530

    Factory Pattern

---
+++ src/main/designpatterns/Factory/FactoryMain.java
@@ -0,0 +1,16 @@
+package main.designpatterns.Factory;
+
+import main.designpatterns.Factory.Components.Button.Button;
+import main.designpatterns.Factory.Components.Dropdown.Dropdown;
+import main.designpatterns.Factory.Components.Menu.Menu;
+
+import java.nio.Buffer;
+
+public class FactoryMain {
+    public static void main(String[] args) {
+        UIFactory uiFactory = UIFactoryFactory.getUIFactory(SupportedPlateforms.IOS);
+        Button button = uiFactory.createButton();
+        Menu menu = uiFactory.createMenu();
+        Dropdown dropdown = uiFactory.createDropdown();
+    }
+}
